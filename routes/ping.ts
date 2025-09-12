3d7ecc4737eddbe7b6f590910133b1a76d38ef14
Sagar Saini <94279952+Sagar141005@users.noreply.github.com>
2025-09-12 23:06:57 +0530

    Update ping.ts

--- routes/ping.ts
+++ routes/ping.ts
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "✅ Pong! Server is alive." });
 });
 
+router.get("/keep-alive", (_, res) => {
+  res.send("OK");
+});
+
 export default router;
